fdcd734ce8a291960d05d0ca65375f06604d6dac
tenaciousgeo <tenaciousgeo@gmail.com>
2014-04-22 15:03:19 -0700

    Update DetectOSArch.bat

--- DetectOSArch.bat
+++ DetectOSArch.bat
@@ -1,6 +1,6 @@
-    @ECHO Off
+@ECHO Off
 
-    REM ======== BEGIN SET VARIABLES ===========
+REM ======== BEGIN SET VARIABLES ===========
     REM Set the myArch variable with x64 or x86
     IF %PROCESSOR_ARCHITECTURE% == AMD64 SET myArch=x64
     IF %PROCESSOR_ARCHITECTURE% == x86 SET myArch=x86
